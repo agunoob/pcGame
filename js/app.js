a6f0334da068f0903b1cbc62a2861da22c547a63
agunoob <74703735+agunoob@users.noreply.github.com>
2021-03-25 21:22:30 +0100

    next commit
    move closer and away
    walk to start and end of screen fixed

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const character = document.querySelector('.character');
 let position = 0;
 let bigger = 1;
+let jumpPosition = 96;
 
 document.onkeydown = characterGo;
 
@@ -19,6 +20,7 @@ function characterGo(e)
         {
             position = window.innerWidth - 90;
             console.log('stop right');
+            character.style.transform = 'translateX(' + position + 'px)';
         }
     };
 
@@ -34,22 +36,55 @@ function characterGo(e)
         {
             position = -10;
             console.log('stop left');
+            character.style.transform = 'translateX(' + position + 'px)';
         }
     };
     console.log(position)
 
-    // //closer
-    // if (e.keyCode == '40')
-    // {
-    //     bigger = bigger + 0.01;
-    //     character.style.transform += 'scale(' + bigger + ')';
-    // }
+    //closer
+    if (e.keyCode == '40')
+    {
+        if (bigger >= 20)
+        {
+            bigger == 20
+        }
+        else
+        {
+            bigger = bigger + 1;
+            jumpPosition = jumpPosition - 5;
+            console.log(bigger);
+            character.style.width = 'calc(70px + ' + bigger + 'px)';
+            character.style.bottom = jumpPosition + 'px';
+            const stepCloser = new Audio('./sounds/step.mp3');
+            stepCloser.play();
+        }
+    }
+
+        //away
+        if (e.keyCode == '38')
+        {
+            if (bigger == 1)
+            {
+                bigger == 1
+            }
+            else
+            {
+                bigger = bigger - 1;
+                jumpPosition = jumpPosition + 6;
+                console.log(bigger);
+                character.style.width = 'calc(70px + ' + bigger + 'px)';
+                character.style.bottom = jumpPosition + 'px';
+                const stepCloser = new Audio('./sounds/step.mp3');
+                stepCloser.play();
+            }
+        }
 
     //jump
     if (e.keyCode == '32')
     {
         console.log('spacja');
-        character.style.bottom = '120px';
+        // character.style.bottom = '120px';
+        character.style.bottom = 'calc('+ jumpPosition + 'px + 24px';
         const jumpSound = new Audio('./sounds/jump.mp3');
         jumpSound.play();
 
@@ -57,7 +92,8 @@ function characterGo(e)
         {
             setTimeout(function()
             {
-                character.style.bottom = '96px';
+                // character.style.bottom = '96px';
+                character.style.bottom = jumpPosition + 'px';
             }, 250);
         }
         waitToFall();
