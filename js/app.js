f43b1ef4b32896d02aec023f21b7359d343fdeb0
agunoob <74703735+agunoob@users.noreply.github.com>
2021-03-26 11:49:37 +0100

    Add files via upload
    go away - back picture
    code fixes

--- js/app.js
+++ js/app.js
@@ -4,44 +4,84 @@ let position = 0;
 let bigger = 1;
 let jumpPosition = 96;
 
-document.onkeydown = characterGo;
+document.onkeydown = characterMove;
 
-function characterGo(e)
+function characterMove(e)
 {
-    //right
+    goRight(e);
+    goLeft(e);
+    goAway(e);
+    goCloser(e);
+    jump(e);
+    console.log(position);
+};
+
+function goRight(e)
+{
     if (e.keyCode == '39')
     {
-        position = position + 20;
-        character.src = './pictures/character-black-eye.png';
-        character.style.transform = 'translateX(' + position + 'px)';
-        const stepRight = new Audio('./sounds/step.mp3');
-        stepRight.play();
-        if (position >= window.innerWidth - 90)
+        if (position >= window.innerWidth - 100)
         {
-            position = window.innerWidth - 90;
+            position = window.innerWidth - 100;
             console.log('stop right');
             character.style.transform = 'translateX(' + position + 'px)';
         }
-    };
-
-    //left
-    if (e.keyCode == '37')
-    {
-        position = position - 20;
-        character.src = './pictures/character-black-eye-right.png';
-        character.style.transform = 'translateX(' + position + 'px)';
-        const stepLeft = new Audio('./sounds/step.mp3');
-        stepLeft.play();
-        if (position <= -10)
+        else
         {
-            position = -10;
+            position = position + 20;
+            character.src = './pictures/character-black-eye.png';
+            character.style.transform = 'translateX(' + position + 'px)';
+            const stepRight = new Audio('./sounds/step.mp3');
+            stepRight.play();
+        }
+    }
+};
+
+function goLeft(e)
+{
+   if (e.keyCode == '37')
+    {
+        if (position <= -20)
+        {
+            position = -20;
             console.log('stop left');
             character.style.transform = 'translateX(' + position + 'px)';
         }
-    };
-    console.log(position)
+        else
+        {
+            position = position - 20;
+            character.src = './pictures/character-black-eye-right.png';
+            character.style.transform = 'translateX(' + position + 'px)';
+            const stepLeft = new Audio('./sounds/step.mp3');
+            stepLeft.play();
+        }
+    }
+};
 
-    //closer
+function goAway(e)
+{
+    if (e.keyCode == '38')
+    {
+        if (bigger == 1)
+        {
+            bigger == 1
+        }
+        else
+        {
+            bigger = bigger - 1;
+            jumpPosition = jumpPosition + 6;
+            console.log(bigger);
+            character.src = './pictures/character-back.png';
+            character.style.width = 'calc(70px + ' + bigger + 'px)';
+            character.style.bottom = jumpPosition + 'px';
+            const stepCloser = new Audio('./sounds/step.mp3');
+            stepCloser.play();
+        }
+    }
+};
+
+function goCloser(e)
+{
     if (e.keyCode == '40')
     {
         if (bigger >= 15)
@@ -53,37 +93,20 @@ function characterGo(e)
             bigger = bigger + 1;
             jumpPosition = jumpPosition - 6;
             console.log(bigger);
+            character.src = './pictures/character-black-eye-right.png';
             character.style.width = 'calc(70px + ' + bigger + 'px)';
             character.style.bottom = jumpPosition + 'px';
             const stepCloser = new Audio('./sounds/step.mp3');
             stepCloser.play();
         }
     }
+};
 
-        //away
-        if (e.keyCode == '38')
-        {
-            if (bigger == 1)
-            {
-                bigger == 1
-            }
-            else
-            {
-                bigger = bigger - 1;
-                jumpPosition = jumpPosition + 6;
-                console.log(bigger);
-                character.style.width = 'calc(70px + ' + bigger + 'px)';
-                character.style.bottom = jumpPosition + 'px';
-                const stepCloser = new Audio('./sounds/step.mp3');
-                stepCloser.play();
-            }
-        }
-
-    //jump
+function jump(e)
+{
     if (e.keyCode == '32')
     {
         console.log('spacja');
-        // character.style.bottom = '120px';
         character.style.bottom = 'calc('+ jumpPosition + 'px + 24px';
         const jumpSound = new Audio('./sounds/jump.mp3');
         jumpSound.play();
@@ -92,10 +115,9 @@ function characterGo(e)
         {
             setTimeout(function()
             {
-                // character.style.bottom = '96px';
                 character.style.bottom = jumpPosition + 'px';
             }, 250);
         }
         waitToFall();
-    };
-}
+    }
+};
